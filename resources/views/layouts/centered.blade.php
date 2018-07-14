a3120b3c2b9dea5378dde01f85680d58bcc1a51e
Bayu Hendra Winata <uyab.exe@gmail.com>
2018-07-14 13:26:46 +0700

    add new layout: centered (vertically and horizontally)

---
+++ resources/views/layouts/centered.blade.php
@@ -0,0 +1,22 @@
+@extends('ui::layouts.base')
+@section('body')
+    <div style="
+        display: flex;
+        align-items: center;
+        min-height: 100vh;
+        justify-content: center;"
+    >
+        <div style="flex: 1;">
+            <div class="ui centered stackable grid">
+                <div class="column six wide center aligned">
+                    <div class="ui segment very padded container text">
+                        <h1 class="ui header">{{ config('app.name') }}</h1>
+                        <div class="ui divider hidden"></div>
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
