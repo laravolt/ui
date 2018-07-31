243201fa9c737dccb789f11dfd17f89fc5d58b1b
Bayu Hendra Winata <uyab.exe@gmail.com>
2018-08-01 06:14:52 +0700

    styling topbar

--- resources/views/menu/topbar.blade.php
+++ resources/views/menu/topbar.blade.php
@@ -9,37 +9,40 @@
             {{--<div class="active section">T-Shirt</div>--}}
         {{--</div>--}}
     {{--</div>--}}
-    <div class="menu right">
-        <div class="item ui dropdown simple right">
-            <i class="icon bell"></i>
-            <div class="menu notification">
-                <div class="ui comments">
-                    <h4 class="ui divider horizontal p-1">Notifikasi</h4>
-                    @foreach(range(1,5) as $i)
-                        <div class="p-x-1 m-b-1">
-                            <a class="comment" href="#">
-                                <div class="avatar">
-                                    <img src="{{ asset('img/avatar.jpg') }}">
-                                </div>
-                                <div class="content">
-                                    <span>New member joined</span>
-                                    <div class="metadata">
-                                        <span class="date">Today at 5:42PM</span>
-                                    </div>
+    <div class="menu right p-r-1">
+        {{--<div class="item ui dropdown simple right">--}}
+            {{--<i class="icon bell"></i>--}}
+            {{--<div class="menu notification">--}}
+                {{--<div class="ui comments">--}}
+                    {{--<h4 class="ui divider horizontal p-1">Notifikasi</h4>--}}
+                    {{--@foreach(range(1,5) as $i)--}}
+                        {{--<div class="p-x-1 m-b-1">--}}
+                            {{--<a class="comment" href="#">--}}
+                                {{--<div class="avatar">--}}
+                                    {{--<img src="{{ asset('img/avatar.jpg') }}">--}}
+                                {{--</div>--}}
+                                {{--<div class="content">--}}
+                                    {{--<span>New member joined</span>--}}
+                                    {{--<div class="metadata">--}}
+                                        {{--<span class="date">Today at 5:42PM</span>--}}
+                                    {{--</div>--}}
 
-                                    <div class="text">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    @endforeach
-                </div>
-                <a href="" class="item footer text-center">Lihat Semua</a>
-            </div>
+                                    {{--<div class="text">--}}
+                                        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit.--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</div>--}}
+                    {{--@endforeach--}}
+                {{--</div>--}}
+                {{--<a href="" class="item footer text-center">Lihat Semua</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        <div class="item">
         </div>
         <div class="ui item dropdown simple right">
             <img src="{{ \Laravolt\Avatar\Facade::create(auth()->user()->name)->toBase64() }}" alt="" class="ui image avatar">
+            {{ auth()->user()->name }}
             <i class="icon dropdown"></i>
             <div class="menu">
                 <a href="{{ route('auth::logout') }}" class="item">Logout</a>
