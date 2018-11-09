9bbd2dd5192dd02feaadc2ad73d88fc140b2aaad
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-11-09 20:25:05 +0700

    commented out sample menu

--- config/menu.php
+++ config/menu.php
@@ -5,22 +5,19 @@ return [
     /**
      * Example Menu
      */
-    'Main Menu' => [
-        'menu' => [
-            // Menu 1
-            'Menu 1' => [
-                'data' => [
-                    'icon' => 'circle outline'
-                ],
-                // Sub Menu 1-*
-                'menu' => [
-                    'Sub Menu 1-1' => ['url' => '#'],
-                    'Sub Menu 1-2' => '#', // string value is pointing to url.
-                    'Sub Menu 1-3' => '#',
-                    ]
-                ],
-            // Menu 2
-            'Menu 2' => ['url' => '#', 'data' => ['icon' => 'circle outline']],
-        ]
-    ],
+    // 'Main Menu' => [
+    //     'menu' => [
+    //         'Menu 1' => [
+    //             'data' => [
+    //                 'icon' => 'circle outline',
+    //             ],
+    //             'menu' => [
+    //                 'Sub Menu 1-1' => ['url' => '#'],
+    //                 'Sub Menu 1-2' => '#', // string value is pointing to url.
+    //                 'Sub Menu 1-3' => '#',
+    //             ],
+    //         ],
+    //         'Menu 2' => ['url' => '#', 'data' => ['icon' => 'circle outline']],
+    //     ],
+    // ],
 ];
